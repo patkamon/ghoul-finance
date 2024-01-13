d2de509752e780dbceac26e75b0eb153be48a2a1
patkamon <patkamon.a@ku.th>
2024-01-13 21:43:00 +0700

    Add all top ui

--- src/app/page.js
+++ src/app/page.js
@@ -77,14 +77,22 @@ export default function Home() {
       <Navbar />
 
       <div className="bg-purple1 py-24  w-full"></div>
-      <div className="bg-white py-16  w-3/5 relative -my-32 grid grid-cols-3 gap-4 rounded-xl">
-        <div className="m-auto">Profile</div>
+      <div className="bg-white py-8  w-3/5 relative -my-32 grid grid-cols-3 gap-4 rounded-xl">
+        <div className="mx-auto w-full flex flex-col ml-10 -mb-4">
+          <img src="ghoul.png" className="mx-auto -mt-8"></img>
+          <div className="mx-auto -mt-4 font-medium">TIER: 1</div>
+          <button className="bg-purple1 rounded-xl w-1/2 mx-auto text-white text-xl">
+            UPGRADE
+          </button>
+        </div>
 
         <div className="flex flex-col space-y-4 col-span-2 justify-center">
-          <div className="m-auto">STAKED: 102 BORROW: 40</div>
+          <div className="mx-auto text-2xl font-medium">
+            STAKED: 102 BORROW: 40
+          </div>
           <div
             id="myProgress"
-            className="w-3/4 m-auto rounded-lg flex bg-[#D9D9D9]"
+            className="w-3/4 m-auto rounded-lg  flex bg-[#D9D9D9]"
           >
             <div id="myBar" className="bg-green h-8 w-[61%] rounded-lg"></div>
             <div className="ml-auto mr-2 mt-1">61%</div>
