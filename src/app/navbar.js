511f96bed1e994dc0d7308291237361319df3385
patkamon <patkamon.a@ku.th>
2024-01-13 20:33:43 +0700

    Add navbar

---
+++ src/app/navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="bg-purple1  flex py-2 justify-between w-full border">
+      <div className="text-white"> Ghoul</div>
+      <div className="text-white"> Ghoul</div>
+    </div>
+  );
+}
