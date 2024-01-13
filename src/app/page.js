b43b3ddbd6e9833321e11bfe4777cf082e262c63
patkamon <patkamon.a@ku.th>
2024-01-13 20:50:28 +0700

    add progress bar

--- src/app/page.js
+++ src/app/page.js
@@ -80,12 +80,18 @@ export default function Home() {
       <div className="bg-white py-16  w-3/5 relative -my-32 grid grid-cols-3 gap-4 rounded-xl">
         <div className="m-auto">Profile</div>
 
-        <div className="  flex flex-col space-y-4 col-span-2 justify-center">
+        <div className="flex flex-col space-y-4 col-span-2 justify-center">
           <div className="m-auto">STAKED: 102 BORROW: 40</div>
-          <div className="m-auto">BAR</div>
+          <div
+            id="myProgress"
+            className="w-3/4 m-auto rounded-lg flex bg-[#D9D9D9]"
+          >
+            <div id="myBar" className="bg-green h-8 w-[61%] rounded-lg"></div>
+            <div className="ml-auto mr-2 mt-1">61%</div>
+          </div>
           <div className="z-0 flex gap-4 justify-center">
             <button
-              className="bg-purple2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+              className="bg-purple2 hover:bg-blue-700 text-[#897E7E] font-bold py-2 px-4 rounded-xl"
               onClick={(e) => {
                 supply(e);
               }}
@@ -93,7 +99,7 @@ export default function Home() {
               Supply
             </button>
             <button
-              className="bg-purple2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+              className="bg-purple2 hover:bg-blue-700 text-[#897E7E] font-bold py-2 px-4 rounded-xl"
               onClick={(e) => {
                 withdraw(e);
               }}
@@ -101,7 +107,7 @@ export default function Home() {
               Withdraw
             </button>
             <button
-              className="bg-purple2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+              className="bg-purple2 hover:bg-blue-700 text-[#897E7E] font-bold py-2 px-4 rounded-xl"
               onClick={(e) => {
                 borrow(e);
               }}
