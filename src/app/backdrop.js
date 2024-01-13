3896d9a6cf6ce54142ecb4906c4c88706fffed61
patkamon <patkamon.a@ku.th>
2024-01-14 00:42:40 +0700

    fix

--- src/app/backdrop.js
+++ src/app/backdrop.js
@@ -1,3 +1,5 @@
 export default function Backdrop() {
-  return <div className="bg-purple1 py-24  w-full top-10 fixed"></div>;
+  return (
+    <div className="bg-purple1 py-24  w-full top-10 fixed overflow-y-scroll"></div>
+  );
 }
