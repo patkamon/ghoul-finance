ddcd77987fdcd4bd610bac14fe534ca7646a5675
patkamon <patkamon.a@ku.th>
2024-01-13 19:18:57 +0700

    add borrow withdraw

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,16 @@ import poolContract from "@aave/core-v3/artifacts/contracts/interfaces/IPool.sol
 
 export default function Home() {
   const usdcAddress = "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8"; // mainnet usdc
+  const ghoAddress = "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60";
   const referralCode = "0";
   const userAddress = "0xF0A94EC0F27203C399e17d5533A77e00F9813450";
-
   const poolAddress = "0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951"; // mainnet address, for other addresses: https://docs.aave.com/developers/developing-on-aave/deployed-contract-instances
 
   async function requestAccount() {
     await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
-  async function getPool(e) {
+  async function supply(e) {
     e.preventDefault();
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
@@ -33,16 +33,75 @@ export default function Home() {
     }
   }
 
+  async function withdraw(e) {
+    e.preventDefault();
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        poolAddress,
+        poolContract.abi,
+        signer
+      );
+      try {
+        contract.withdraw(usdcAddress, "10000000", userAddress);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
+  async function borrow(e) {
+    e.preventDefault();
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        poolAddress,
+        poolContract.abi,
+        signer
+      );
+      try {
+        contract.borrow(ghoAddress, "10000000000000000", "2", "0", userAddress);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-yellow-400">Ghoul</div>
-      <button
-        onClick={(e) => {
-          getPool(e);
-        }}
-      >
-        Borrow
-      </button>
+      <div ckassName="">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={(e) => {
+            supply(e);
+          }}
+        >
+          Supply
+        </button>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={(e) => {
+            withdraw(e);
+          }}
+        >
+          Withdraw
+        </button>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={(e) => {
+            borrow(e);
+          }}
+        >
+          Borrow
+        </button>
+      </div>
     </main>
   );
 }
